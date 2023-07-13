39b87ae2c8045247dae75d62f9c8940ec450c610
Rafael da Silva <rfsrafaelsilva@gmail.com>
2023-07-13 20:43:18 -0300

    Aulas de Operadores

---
+++ operadores_associacao.py
@@ -0,0 +1,9 @@
+#Operador Associação
+curso = "Curso Python"
+frutas = ['Laranja', 'banana', 'macã']
+saques = [1500 , 100]
+
+print("Python" in curso)
+print('Pera' in frutas)
+print(200 in saques)
+print('Ameixa' not in frutas)
---
+++ operadores_atribuicao.py
@@ -0,0 +1,25 @@
+#Operadores Atribuição
+
+#atribuição Simples
+saldo = 500
+print(f'Atribuindo simples de 500 ao saldo = {saldo}')
+#atribuição com Adição
+
+saldo += 200
+print(f'Atruibuição com adição = {saldo}')
+#atribuição com subtração
+
+saldo -= 200
+print(f'Atruibuição com subtração = {saldo}')
+#atribuição com divisão casas
+
+saldo /= 2
+print(f'Atruibuição com divisão casas = {saldo}')
+#atribuição com divisão inteira
+
+saldo //= 2
+print(f'Atruibuição com divisão inteira = {saldo}')
+#atribuição com exponeciação
+
+saldo **= 2
+print(f'Atruibuição com exponenciação = {saldo}')
